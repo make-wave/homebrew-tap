9c29f0adce576a96dfae7a64ce311c9f606b93cb
Gary Holland <21319237+hollg@users.noreply.github.com>
2025-08-01 15:37:34 +0100

    Revert "Update wave formula to 0.0.2"

--- Formula/wave.rb
+++ Formula/wave.rb
@@ -1,10 +1,10 @@
 class Wave < Formula
   desc "Your app description"
   homepage "https://github.com/make-wave/wave"
-  url "https://github.com/make-wave/wave/releases/download/v0.0.2/wave-macos-latest"
-  sha256 "4b81d36ade6cb72572b7dec5582482f426b9758ad14b02056d25208c895d4113"
+  url "https://github.com/make-wave/wave/releases/download/v0.0.1/wave-macos-latest"
+  sha256 "c6fbdc78a55352f6bdd68297bcf917b9dc6c1e2d528c87c50c0b2021f37b4ce2"
   license "MIT"
-  version "0.0.2"
+  version "0.0.1"
 
   def install
     bin.install "wave-macos-latest" => "wave"
